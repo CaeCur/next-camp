1a33bf1e51720f3a549ed3a7b43b5c9f4ea7dc97
Caelan <caelan.curry@gmail.com>
2022-04-12 15:33:10 +0100

    Added starting pages for init

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Uh Oh...</h1>
+      <p>Page not found</p>
+    </div>
+  );
+}
---
+++ pages/campgrounds/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Campgrounds() {
+  return (
+    <div>
+      <h1>Campgrounds List</h1>
+    </div>
+  );
+}
